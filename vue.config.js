d982a15236421aff923eabd8d9266b2fa39fef6c
Fernando Moreira <nandomoreira.me@gmail.com>
2019-05-07 19:16:48 -0300

    upgrade packages

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  lintOnSave: false,
+  lintOnSave: true,
   devServer: {
     host: '0.0.0.0',
     hot: true,
     disableHostCheck: true
+  },
+  pwa: {
+    name: 'app-whatsapp-url'
   }
 }
